856f6b03512131c55700ad06a0020c0aa2427490
Drexler Reyes <2xe2ipi@gmail.com>
2026-02-02 02:11:19 +0800

    hahahha

--- server/gameData.js
+++ server/gameData.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         id: 1,
         type: 'image',
-        content: '/assets/q4.webp',
+        content: '/assets/q1.webp',
         question: "Real or AI?",
         answer: 'AI' 
     },
@@ -18,7 +18,7 @@ module.exports = [
         type: 'image',
         content: '/assets/q3.webp',
         question: "Real or AI?",
-        answer: 'AI' 
+        answer: 'HUMAN' 
     },
     {
         id: 4,
@@ -37,8 +37,72 @@ module.exports = [
     {
         id: 6,
         type: 'image',
-        content: '/assets/q5.webp',
+        content: '/assets/q6.webp',
         question: "Real or AI?",
         answer: 'HUMAN' 
-    }
+    },
+    {
+        id: 7,
+        type: 'image',
+        content: '/assets/q7.webp',
+        question: "Real or AI?",
+        answer: 'HUMAN' 
+    },
+    {
+        id: 8,
+        type: 'image',
+        content: '/assets/q8.webp',
+        question: "Real or AI?",
+        answer: 'AI' 
+    },
+    {
+        id: 9,
+        type: 'image',
+        content: '/assets/q9.webp',
+        question: "Real or AI?",
+        answer: 'AI' 
+    },
+    {
+        id: 10,
+        type: 'image',
+        content: '/assets/q10.webp',
+        question: "Real or AI?",
+        answer: 'AI' 
+    },
+    {
+        id: 11,
+        type: 'image',
+        content: '/assets/q11.webp',
+        question: "Real or AI?",
+        answer: 'HUMAN' 
+    },
+    {
+        id: 12,
+        type: 'image',
+        content: '/assets/q12.webp',
+        question: "Real or AI?",
+        answer: 'AI' 
+    },
+    {
+        id: 13,
+        type: 'image',
+        content: '/assets/q13.webp',
+        question: "Real or AI?",
+        answer: 'HUMAN' 
+    },
+    {
+        id: 14,
+        type: 'image',
+        content: '/assets/q14.webp',
+        question: "Real or AI?",
+        answer: 'HUMAN' 
+    },
+    {
+        id: 15,
+        type: 'image',
+        content: '/assets/q15.webp',
+        question: "Real or AI?",
+        answer: 'AI' 
+    },
+    
 ];
